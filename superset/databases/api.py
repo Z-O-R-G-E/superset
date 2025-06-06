30c84b0203b1f1bf4a9db59ab92246b73f71589d
paulz <z0rge@bk.ru>
2025-06-06 22:53:08 +0300

    refactoring

--- superset/databases/api.py
+++ superset/databases/api.py
@@ -1785,8 +1785,8 @@ class DatabaseRestApi(BaseSupersetModelRestApi):
             parameters = FieldsUploadPostSchema().load(request_form)
             FieldsUploadCommand(
                 pk,
-                parameters["table_name"],
-                parameters["upload_fields"],
+                parameters["table"],
+                parameters["uploadFields"],
                 parameters.get("schema"),
                 FieldsReader(parameters),
             ).run()
