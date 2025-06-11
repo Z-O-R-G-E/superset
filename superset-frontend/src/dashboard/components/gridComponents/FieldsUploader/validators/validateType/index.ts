06cd3954dd26da13088c88cb11618cc2cb1c1727
paulz <z0rge@bk.ru>
2025-06-11 11:00:53 +0300

    fix interval client validation

--- superset-frontend/src/dashboard/components/gridComponents/FieldsUploader/validators/validateType/index.ts
+++ superset-frontend/src/dashboard/components/gridComponents/FieldsUploader/validators/validateType/index.ts
@@ -81,7 +81,7 @@ const REGEX = {
   HEX: /^[0-9A-Fa-f]+$/,
   IPV4: /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/,
   IPV6: /^((([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4})|(([0-9a-fA-F]{1,4}:){1,7}:)|(([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4})|(([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2})|(([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3})|(([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4})|(([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5})|([0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))|(:((:[0-9a-fA-F]{1,4}){1,7}|:))|(fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,})|(::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))|(([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))$/,
-  INTERVAL: /^(\d+ )?\d{2}:\d{2}:\d{2}(\.\d+)?$/,
+  INTERVAL: /^(\d+\s+)?\d{1,2}:\d{1,2}:\d{1,2}(\.\d+)?$/,
   GEOJSON: /^{"type":"[A-Za-z]+","coordinates":/,
   GEOMETRY: /^[A-Z]+\s*\(.*\)$/,
   ORACLE_NUMBER: /^-?\d+(\.\d+)?$/,
@@ -543,8 +543,24 @@ export const validateType =
 
       case 'INTERVAL': {
         if (!REGEX.INTERVAL.test(stringValue)) {
-          return error('Формат интервала: [DD ]HH:MM:SS[.миллисекунды]');
+          return error('Формат интервала: [DD ]HH:MM:SS[.дробные_секунды]');
         }
+
+        const [daysPart, timePart] = stringValue.split(/\s+/);
+        let hours;
+        let minutes;
+        let seconds;
+
+        if (timePart) {
+          [hours, minutes, seconds] = timePart.split(':').map(Number);
+        } else {
+          [hours, minutes, seconds] = daysPart.split(':').map(Number);
+        }
+
+        if (hours >= 24) return error('Часы должны быть меньше 24');
+        if (minutes >= 60) return error('Минуты должны быть меньше 60');
+        if (seconds >= 60) return error('Секунды должны быть меньше 60');
+
         break;
       }
 
