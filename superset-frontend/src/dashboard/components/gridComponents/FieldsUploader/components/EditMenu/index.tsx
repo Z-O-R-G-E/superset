428896ea48e083c3d5ffb50bc539caa084f99ec8
paulz <z0rge@bk.ru>
2025-06-23 22:45:35 +0300

    refactoring EditMenu

--- superset-frontend/src/dashboard/components/gridComponents/FieldsUploader/components/EditMenu/index.tsx
+++ superset-frontend/src/dashboard/components/gridComponents/FieldsUploader/components/EditMenu/index.tsx
@@ -6,47 +6,42 @@ import { DataWarehouse } from '../DataWarehouse';
 import { ColumnsProperties } from '../ColumnsProperties';
 import UploadFields from '../UploadFields';
 
+const PANELS = [
+  {
+    key: '1',
+    title: t('Заголовок'),
+    component: <HeaderSettings />,
+  },
+  {
+    key: '2',
+    title: t('Хранилище данных'),
+    component: <DataWarehouse />,
+  },
+  {
+    key: '3',
+    title: t('Параметры колонок'),
+    component: <ColumnsProperties />,
+  },
+  {
+    key: '4',
+    title: t('Поля для загрузки'),
+    component: <UploadFields />,
+  },
+];
+
 export const EditMenu: FC = memo(() => (
   <Collapse defaultActiveKey={1} accordion ghost>
-    <Collapse.Panel
-      header={
-        <Divider orientation="left" style={{ margin: 0 }}>
-          {t('Заголовок')}
-        </Divider>
-      }
-      key="1"
-    >
-      <HeaderSettings />
-    </Collapse.Panel>
-    <Collapse.Panel
-      header={
-        <Divider orientation="left" style={{ margin: 0 }}>
-          {t('Хранилище данных')}
-        </Divider>
-      }
-      key="2"
-    >
-      <DataWarehouse />
-    </Collapse.Panel>
-    <Collapse.Panel
-      header={
-        <Divider orientation="left" style={{ margin: 0 }}>
-          {t('Параметры колонок')}
-        </Divider>
-      }
-      key="3"
-    >
-      <ColumnsProperties />
-    </Collapse.Panel>
-    <Collapse.Panel
-      header={
-        <Divider style={{ margin: 0 }} orientation="left">
-          {t('Поля для загрузки')}
-        </Divider>
-      }
-      key="4"
-    >
-      <UploadFields />
-    </Collapse.Panel>
+    {PANELS.map(({ key, title, component }) => (
+      <Collapse.Panel
+        key={key}
+        header={
+          <Divider orientation="left" style={{ margin: 0 }}>
+            {title}
+          </Divider>
+        }
+      >
+        {component}
+      </Collapse.Panel>
+    ))}
   </Collapse>
 ));
