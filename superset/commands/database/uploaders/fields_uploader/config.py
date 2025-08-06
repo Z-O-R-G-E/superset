cf8449232aafd6ca6d8fe59c678ae2202941c6fe
paulz <z0rge@bk.ru>
2025-08-06 11:57:01 +0300

    add sqlalchemy to config

--- superset/commands/database/uploaders/fields_uploader/config.py
+++ superset/commands/database/uploaders/fields_uploader/config.py
@@ -2,6 +2,7 @@
 TYPE_CONFIG = {
     "integer": {
         "pandas": "Int64",
+        "sqlalchemy": "BigInteger",
         "handler": "IntegerHandler",
         "db_types": {
             "postgresql": {
@@ -32,23 +33,11 @@ TYPE_CONFIG = {
             },
             "*": "BIGINT"
         },
-        "sqlalchemy_types": {
-            "TINYINT": "SmallInteger",
-            "SMALLINT": "SmallInteger",
-            "INT2": "SmallInteger",
-            "INT": "Integer",
-            "INTEGER": "Integer",
-            "BIGINT": "BigInteger",
-            "INT16": "SmallInteger",
-            "INT32": "Integer",
-            "INT64": "BigInteger",
-            "UINT8": "BigInteger",
-            "*": "BigInteger"
-        },
-        "aliases": ["TINYINT", "SMALLINT", "INT2", "INT", "INTEGER", "BIGINT", "UINT8", "INT16", "INT32", "INT64"]
+        "aliases": ["TINYINT", "SMALLINT", "INT2", "INT", "INTEGER", "BIGINT", "INT16", "INT32", "INT64"]
     },
     "float": {
         "pandas": "float64",
+        "sqlalchemy": "Float",
         "handler": "FloatHandler",
         "db_types": {
             "postgresql": {
@@ -73,41 +62,30 @@ TYPE_CONFIG = {
             },
             "*": "DOUBLE PRECISION"
         },
-        "sqlalchemy_types": {
-            "FLOAT4": "Float",
-            "FLOAT32": "Float",
-            "FLOAT": "Float",
-            "FLOAT8": "Float",
-            "FLOAT64": "Float",
-            "DOUBLE": "Float",
-            "REAL": "Float",
-            "*": "Float"
-        },
         "precision_mapping": {
             "FLOAT4": 24,
             "FLOAT32": 24,
             "REAL": 24,
             "*": 53
         },
-        "aliases": ["FLOAT", "FLOAT4", "FLOAT8", "FLOAT32", "FLOAT64", "DOUBLE", "REAL", "BINARY_FLOAT", "BINARY_DOUBLE"]
+        "aliases": ["FLOAT", "FLOAT4", "FLOAT8", "FLOAT32", "FLOAT64", "DOUBLE", "REAL"]
     },
     "decimal": {
         "pandas": "object",
+        "sqlalchemy": "Numeric",
         "handler": "DecimalHandler",
         "db_types": {
-            "postgresql": "NUMERIC",
-            "clickhouse": "Decimal",
-            "*": "NUMERIC"
-        },
-        "sqlalchemy_types": {
-            "*": "Numeric"
+            "postgresql": "NUMERIC({precision}, {scale})",
+            "clickhouse": "Decimal({precision}, {scale})",
+            "*": "NUMERIC({precision}, {scale})"
         },
         "default_precision": 18,
         "default_scale": 4,
-        "aliases": ["DECIMAL", "NUMERIC", "NUMBER"]
+        "aliases": ["DECIMAL", "NUMERIC"]
     },
     "string": {
         "pandas": "string",
+        "sqlalchemy": "String",
         "handler": "StringHandler",
         "db_types": {
             "postgresql": {
@@ -118,84 +96,69 @@ TYPE_CONFIG = {
             "clickhouse": {
                 "CHAR": "FixedString({size})",
                 "VARCHAR": "FixedString({size})",
-                "STRING": "FixedString({size})",
+                "STRING": "String",
                 "FIXEDSTRING": "FixedString({size})",
                 "*": "String"
             },
             "*": "TEXT"
         },
-        "sqlalchemy_types": {
-            "CHAR": "CHAR",
-            "VARCHAR": "VARCHAR",
-            "FIXEDSTRING": "VARCHAR",
-            "*": "Text"
-        },
-        "aliases": ["CHAR", "VARCHAR", "TEXT", "NCHAR", "NVARCHAR", "CLOB", "LONGTEXT", "FIXEDSTRING", "STRING"]
+        "default_size": 255,
+        "aliases": ["CHAR", "VARCHAR", "FIXEDSTRING", "STRING", "TEXT"]
     },
     "date": {
         "pandas": "datetime64[ns]",
+        "sqlalchemy": "Date",
         "handler": "DateHandler",
         "db_types": {
             "postgresql": "DATE",
             "clickhouse": "Date",
             "*": "DATE"
         },
-        "sqlalchemy_types": {
-            "*": "Date"
-        },
         "aliases": ["DATE"]
     },
     "time": {
         "pandas": "object",
+        "sqlalchemy": "Time",
         "handler": "TimeHandler",
         "db_types": {
             "postgresql": "TIME",
-            "clickhouse": "DateTime",  # ClickHouse не имеет отдельного типа TIME
+            "clickhouse": "DateTime",
             "*": "TIME"
-        },
-        "sqlalchemy_types": {
-            "*": "Time"
         },
         "aliases": ["TIME"]
     },
     "datetime": {
         "pandas": "datetime64[ns]",
+        "sqlalchemy": "DateTime",
         "handler": "DateTimeHandler",
         "db_types": {
             "postgresql": "TIMESTAMP",
             "clickhouse": "DateTime",
             "*": "TIMESTAMP"
         },
-        "sqlalchemy_types": {
-            "*": "DateTime"
-        },
-        "aliases": ["DATETIME", "TIMESTAMP", "DATETIME64"]
+        "aliases": ["DATETIME", "TIMESTAMP"]
     },
     "datetimetz": {
         "pandas": "datetime64[ns, UTC]",
+        "sqlalchemy": "DateTime(timezone=True)",
         "handler": "DateTimeTzHandler",
         "db_types": {
             "postgresql": "TIMESTAMP WITH TIME ZONE",
-            "clickhouse": "DateTime",  # ClickHouse не поддерживает timezone в типе
+            "clickhouse": "DateTime64(3, 'UTC')",
             "*": "TIMESTAMP WITH TIME ZONE"
         },
-        "sqlalchemy_types": {
-            "*": "DateTime"
-        },
-        "aliases": ["TIMESTAMPTZ"]
+        "aliases": ["TIMESTAMPTZ", "DATETIMETZ"]
     },
     "boolean": {
         "pandas": "boolean",
+        "sqlalchemy": "Boolean",
         "handler": "BooleanHandler",
         "db_types": {
             "postgresql": "BOOLEAN",
             "clickhouse": "UInt8",
             "*": "BOOLEAN"
         },
-        "sqlalchemy_types": {
-            "*": "Boolean"
-        },
-        "aliases": ["BOOLEAN", "BIT", "BOOL", "UINT8"]
+        "aliases": ["BOOLEAN", "BOOL"]
     }
 }
 
@@ -233,6 +196,7 @@ TYPE_MAPPING = {}
 for type_name, config in TYPE_CONFIG.items():
     type_entry = {
         "pandas": config["pandas"],
+        "sqlalchemy": config["sqlalchemy"],
         "handler": config["handler"]
     }
     TYPE_MAPPING[type_name.upper()] = type_entry
