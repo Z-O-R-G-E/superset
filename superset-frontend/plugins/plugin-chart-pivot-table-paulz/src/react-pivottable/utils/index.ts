be061f1c2532c60c606436a34f8f49beeb315d24
paulz <z0rge@bk.ru>
2025-12-05 09:29:52 +0300

    remove unused derivers

--- superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/utils/index.ts
+++ superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/utils/index.ts
@@ -6,4 +6,3 @@ export { getSort } from './getSort';
 export { flatKey } from './flatKey';
 export { aggregatorsFactory } from './aggregatorsFactory';
 export { aggregatorTemplates } from './aggregatorTemplates';
-export { derivers } from './derivers';
