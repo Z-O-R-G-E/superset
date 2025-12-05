7fcf4cc87bacf7980f0d34a604177d8cde6b87b8
paulz <z0rge@bk.ru>
2025-12-05 08:35:25 +0300

    refactoring numberFormat fom js to ts

--- superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/utils/numberFormat/index.ts
+++ superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/utils/numberFormat/index.ts
@@ -1,15 +1,28 @@
-const addSeparators = function (nStr, thousandsSep, decimalSep) {
+const addSeparators = (
+  nStr: number | string,
+  thousandsSep: string,
+  decimalSep: string,
+): string => {
   const x = String(nStr).split('.');
   let x1 = x[0];
   const x2 = x.length > 1 ? decimalSep + x[1] : '';
   const rgx = /(\d+)(\d{3})/;
+
   while (rgx.test(x1)) {
     x1 = x1.replace(rgx, `$1${thousandsSep}$2`);
   }
+
   return x1 + x2;
 };
 
-const numberFormat = function (optsIn) {
+export const numberFormat = (optsIn: {
+  digitsAfterDecimal?: number;
+  scaler?: number;
+  thousandsSep?: string;
+  decimalSep?: string;
+  prefix?: string;
+  suffix?: string;
+}) => {
   const defaults = {
     digitsAfterDecimal: 2,
     scaler: 1,
@@ -18,18 +31,17 @@ const numberFormat = function (optsIn) {
     prefix: '',
     suffix: '',
   };
+
   const opts = { ...defaults, ...optsIn };
-  return function (x) {
+
+  return (x: number): string => {
     if (Number.isNaN(x) || !Number.isFinite(x)) {
       return '';
     }
-    const result = addSeparators(
-      (opts.scaler * x).toFixed(opts.digitsAfterDecimal),
-      opts.thousandsSep,
-      opts.decimalSep,
-    );
+
+    const scaled = (opts.scaler * x).toFixed(opts.digitsAfterDecimal);
+    const result = addSeparators(scaled, opts.thousandsSep, opts.decimalSep);
+
     return `${opts.prefix}${result}${opts.suffix}`;
   };
 };
-
-export { numberFormat };
