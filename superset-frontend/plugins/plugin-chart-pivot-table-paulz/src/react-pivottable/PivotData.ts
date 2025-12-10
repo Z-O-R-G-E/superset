8cd427dd404c8da7ffe2c0bf884903994844aacf
paulz <z0rge@bk.ru>
2025-12-10 09:50:14 +0300

    fix getAggregator

--- superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/PivotData.ts
+++ superset-frontend/plugins/plugin-chart-pivot-table-paulz/src/react-pivottable/PivotData.ts
@@ -79,14 +79,27 @@ export const createPivotData = (
     allTotal,
 
     getAggregator(rowKey: DataRecordValue[], colKey: DataRecordValue[]) {
+      let agg;
       const flatRowKey = flatKey(rowKey);
       const flatColKey = flatKey(colKey);
-
+      if (rowKey.length === 0 && colKey.length === 0) {
+        agg = allTotal;
+      } else if (rowKey.length === 0) {
+        agg = colTotals[flatColKey];
+      } else if (colKey.length === 0) {
+        agg = rowTotals[flatRowKey];
+      } else {
+        agg = tree[flatRowKey][flatColKey];
+      }
       return (
-        tree?.[flatRowKey]?.[flatColKey] ||
-        rowTotals?.[flatRowKey] ||
-        colTotals?.[flatColKey] ||
-        allTotal
+        agg || {
+          value() {
+            return null;
+          },
+          format() {
+            return '';
+          },
+        }
       );
     },
   };
